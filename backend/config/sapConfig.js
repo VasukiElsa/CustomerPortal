74252f16b0ef7933fee135572a18f9b3b84ef10a
VasukiElsa <vasuki.janarthanan6@gmail.com>
2025-07-24 18:18:57 +0530

    Add config urls

--- backend/config/sapConfig.js
+++ backend/config/sapConfig.js
@@ -3,5 +3,9 @@ require('dotenv').config();
 module.exports = {
   sapHost: process.env.SAP_HOST,
   sapUsername: process.env.SAP_USERNAME,
-  sapPassword: process.env.SAP_PASSWORD
+  sapPassword: process.env.SAP_PASSWORD,
+  sapProfileUrl : process.env.SAP_PROFILE_URL,
+  sapInquiryUrl: process.env.SAP_INQUIRY_URL,
+  sapSalesOrderUrl: process.env.SAP_SALESORDER_URL,
+  sapDeliveryUrl: process.env.SAP_DELIVERY_URL
 };
